351c1e5313b7214685b8ead455bfcbf5f1a851ff
vtcong2299 <vtcong2299@gmail.com>
2024-08-05 22:44:16 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ChuyenDoiTienTe
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            float VND;
+            float USD;
+            
+            Console.Write("Nhap vao so tien USD ban muon doi: ");
+            USD = float.Parse(Console.ReadLine());
+            VND = USD*23000;
+            Console.WriteLine("Doi ra duoc {0} Viet Nam Dong", VND);
+        
+        }
+    }
+}
